b6683706199dfdb86c44cd926e60e8ed145d8c27
reza <rezasalehibabarsad@hotmail.com>
2023-08-13 23:12:42 +0330

    Navigation Setup

--- app/src/main/java/com/rezoo/pkemon_compose/MainActivity.kt
+++ app/src/main/java/com/rezoo/pkemon_compose/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.DividerDefaults.color
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SnackbarDefaults.color
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.rezoo.pkemon_compose.ui.theme.PkemonComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,26 +27,36 @@ class MainActivity : ComponentActivity() {
         setContent {
             PkemonComposeTheme {
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "pokemen_list_screen" ){
-                    
+                NavHost(
+                    navController = navController,
+                    startDestination = "pokemen_list_screen"
+                ){
+                    composable("pokemon_list_screen"){
+
+                    }
+                    composable(
+                        "pokemon_detail_screen",
+                        arguments = listOf(
+                            navArgument("dominantColor"){
+                                type= NavType.IntType
+                            },
+                            navArgument("pokemonName"){
+                                type= NavType.StringType
+                            }
+                        )
+                    ){
+                        val dominantColor = remember{
+                            val color = it.arguments?.getInt("dominatColor")
+                            color?.let { Color(it) } ?: Color.White
+                        }
+                        val pokemonName = remember{
+                            it.arguments?.getString("pokemonName")
+                        }
+
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PkemonComposeTheme {
-        Greeting("Android")
-    }
-}
